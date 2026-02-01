84bd4c850beff3a16002117232e4a7e30bc59d65
avirup-codebuddy <avirup@codebuddy.co>
2026-02-01 11:52:05 +0530

    feat: Implement PR Buddy popup header component with app branding, version, theme toggle, and add extension assets.

--- src/popup/components/Header.tsx
+++ src/popup/components/Header.tsx
@@ -11,12 +11,14 @@ export function Header() {
 
   return (
     <header className="flex items-center justify-between border-b border-border px-6 py-4 bg-background sticky top-0 z-10">
-      <div className="flex items-end gap-3 text-foreground">
-        <Icon icon="streamline-plump:ai-edit-robot-solid" className="w-5 h-5" />
+      <div className="flex items-end gap-1.5 text-foreground">
+        <Icon icon="mdi:robot-happy" className="w-6 h-6" />
         {/* <div className="w-8 h-8 rounded-lg flex items-center justify-center text-primary">
         </div> */}
-        <h1 className="text-md font-bold leading-tight">PR Buddy</h1>
-        <p className="text-center text-xs text-muted-foreground">
+        <h1 className="text-lg font-bold leading-tight tracking-[-2px]">
+          PR&nbsp;&nbsp;Buddy
+        </h1>
+        <p className="text-center text-xs text-muted-foreground ml-1">
           v{packageJson.version}
         </p>
       </div>
